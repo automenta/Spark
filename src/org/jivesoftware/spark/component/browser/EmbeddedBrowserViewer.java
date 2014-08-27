5cc31f96f63a5560bee12807df2c5310bea07fcf
sseehh <1s1e1h1@gmail.com>
2014-08-27 01:44:46 -0400

    replaced lobo browser with javafx webview (webkit renderer)

--- src/org/jivesoftware/spark/component/browser/EmbeddedBrowserViewer.java
+++ src/org/jivesoftware/spark/component/browser/EmbeddedBrowserViewer.java
@@ -18,40 +18,37 @@
 package org.jivesoftware.spark.component.browser;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
+import java.net.URL;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.LookAndFeel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import org.lobobrowser.gui.ContentEvent;
-import org.lobobrowser.gui.ContentListener;
-import org.lobobrowser.gui.FramePanel;
-import org.lobobrowser.main.PlatformInit;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-public class EmbeddedBrowserViewer extends BrowserViewer implements ContentListener {
+public class EmbeddedBrowserViewer extends BrowserViewer{
 
     private static final long serialVersionUID = -8055149462713514766L;
-    private final FramePanel browser;
 
     /**
      * Constructs a new LobobrowserViewer
      */
     public EmbeddedBrowserViewer() {
-        LookAndFeel laf = UIManager.getLookAndFeel();
-        try {
-            PlatformInit.getInstance().init(false, false);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        browser = new FramePanel();
-        //substance look and feel 
-        try {
-            UIManager.setLookAndFeel(laf);
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-        browser.addContentListener(this);
+        super();
+        setLayout(new BorderLayout());
     }
 
     /**
@@ -59,41 +56,22 @@ public class EmbeddedBrowserViewer extends BrowserViewer implements ContentListe
      */
     @Override
     public void goBack() {
-        browser.back();
+        //browser.back();
     }
 
-    /**
-     * Initialization of the BrowserViewer
-     */
-    @Override
-    public void initializeBrowser() {
-        this.setLayout(new BorderLayout());
-        this.add(browser, BorderLayout.CENTER);
-    }
+ 
 
-    /**
-     * Load the given URL
-     */
-    @Override
-    public void loadURL(String url) {
-        try {
-            browser.navigate(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * React to an event by updating the address bar
-     */
-    @Override
-    public void contentSet(ContentEvent event) {
-        if (browser == null || browser.getCurrentNavigationEntry() == null) {
-            return;
-        }
-        String url = browser.getCurrentNavigationEntry().getUrl().toExternalForm();
-        documentLoaded(url);
-    }
+//    /**
+//     * React to an event by updating the address bar
+//     */
+//    @Override
+//    public void contentSet(ContentEvent event) {
+//        if (browser == null || browser.getCurrentNavigationEntry() == null) {
+//            return;
+//        }
+//        String url = browser.getCurrentNavigationEntry().getUrl().toExternalForm();
+//        documentLoaded(url);
+//    }
 
     public static void main(String[] args) {
         EmbeddedBrowserViewer viewer = new EmbeddedBrowserViewer();
@@ -107,4 +85,164 @@ public class EmbeddedBrowserViewer extends BrowserViewer implements ContentListe
         frame.setSize(600, 400);
         viewer.loadURL("http://igniterealtime.org");
     }
+    
+ 
+    private final JFXPanel jfxPanel = new JFXPanel();
+    private WebEngine engine;
+ 
+    private final JPanel panel = new JPanel(new BorderLayout());
+    private final JLabel lblStatus = new JLabel();
+
+
+    private final JButton btnGo = new JButton("Go");
+    private final JTextField txtURL = new JTextField();
+    private final JProgressBar progressBar = new JProgressBar();
+ 
+
+    
+    @Override
+    public void initializeBrowser() {
+        createScene();
+ 
+        ActionListener al = new ActionListener() {
+            @Override 
+            public void actionPerformed(ActionEvent e) {
+                loadURL(txtURL.getText());
+            }
+        };
+ 
+        btnGo.addActionListener(al);
+        txtURL.addActionListener(al);
+  
+        progressBar.setPreferredSize(new Dimension(150, 18));
+        progressBar.setStringPainted(true);
+  
+        JPanel topBar = new JPanel(new BorderLayout(5, 0));
+        topBar.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
+        topBar.add(txtURL, BorderLayout.CENTER);
+        topBar.add(btnGo, BorderLayout.EAST);
+ 
+        JPanel statusBar = new JPanel(new BorderLayout(5, 0));
+        statusBar.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
+        statusBar.add(lblStatus, BorderLayout.CENTER);
+        statusBar.add(progressBar, BorderLayout.EAST);
+ 
+        
+        add(topBar, BorderLayout.NORTH);
+        add(jfxPanel, BorderLayout.CENTER);
+        add(statusBar, BorderLayout.SOUTH);
+        
+
+    }
+ 
+    private void createScene() {
+ 
+        Platform.runLater(new Runnable() {
+            @Override 
+            public void run() {
+ 
+                WebView view = new WebView();
+                engine = view.getEngine();
+ 
+//                engine.titleProperty().addListener(new ChangeListener<String>() {
+//                    @Override
+//                    public void changed(ObservableValue<? extends String> observable, String oldValue, final String newValue) {
+//                        SwingUtilities.invokeLater(new Runnable() {
+//                            @Override 
+//                            public void run() {
+//                                SimpleSwingBrowser.this.setTitle(newValue);
+//                            }
+//                        });
+//                    }
+//                });
+// 
+//                engine.setOnStatusChanged(new EventHandler<WebEvent<String>>() {
+//                    @Override 
+//                    public void handle(final WebEvent<String> event) {
+//                        SwingUtilities.invokeLater(new Runnable() {
+//                            @Override 
+//                            public void run() {
+//                                lblStatus.setText(event.getData());
+//                            }
+//                        });
+//                    }
+//                });
+// 
+                engine.locationProperty().addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> ov, String oldValue, final String newValue) {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override 
+                            public void run() {
+                                documentLoaded(newValue);
+                            }
+                        });
+                    }
+                });
+ 
+                engine.getLoadWorker().workDoneProperty().addListener(new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, final Number newValue) {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override 
+                            public void run() {
+                                progressBar.setValue(newValue.intValue());
+                            }
+                        });
+                    }
+                });
+
+//                engine.getLoadWorker()
+//                        .exceptionProperty()
+//                        .addListener(new ChangeListener<Throwable>() {
+// 
+//                            public void changed(ObservableValue<? extends Throwable> o, Throwable old, final Throwable value) {
+//                                if (engine.getLoadWorker().getState() == FAILED) {
+//                                    SwingUtilities.invokeLater(new Runnable() {
+//                                        @Override public void run() {
+//                                            JOptionPane.showMessageDialog(
+//                                                    panel,
+//                                                    (value != null) ?
+//                                                    engine.getLocation() + "\n" + value.getMessage() :
+//                                                    engine.getLocation() + "\nUnexpected error.",
+//                                                    "Loading error...",
+//                                                    JOptionPane.ERROR_MESSAGE);
+//                                        }
+//                                    });
+//                                }
+//                            }
+//                        });
+//
+                jfxPanel.setScene(new Scene(view));
+            }
+        });
+    }
+ 
+    @Override
+    public void loadURL(final String url) {
+        Platform.runLater(new Runnable() {
+            @Override 
+            public void run() {
+                String tmp = toURL(url);
+ 
+                if (tmp == null) {
+                    tmp = toURL("http://" + url);
+                }
+ 
+                engine.load(tmp);
+            }
+        });
+    }
+
+    private static String toURL(String str) {
+        try {
+            return new URL(str).toExternalForm();
+        } catch (MalformedURLException exception) {
+                return null;
+        }
+    }
+
+   
+
+    
 }
